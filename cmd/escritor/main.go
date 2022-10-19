69f3aaed32ebcfe06e457b761307a64fc1aedbc9
hector toral <hectortoraltrabajos@gmail.com>
2022-10-19 10:18:39 +0200

    cmd

--- cmd/escritor/main.go
+++ cmd/escritor/main.go
@@ -1,5 +1,23 @@
 package main
 
-func main() {
+import (
+	"fmt"
+	"os"
+	"sisdis-pr-2/cmd"
+)
 
+func EscribirFichero(fragmento string) {
+	file, err := os.OpenFile("file.txt", os.O_APPEND, 0600)
+	cmd.CheckError(err)
+	defer file.Close()
+		
+	len, err := file.WriteString(fragmento)
+	cmd.CheckError(err)
+
+	fmt.Printf("\nLength: %d bytes", len)
+}
+
+
+func main() {
+	EscribirFichero("Hola mundo")
 }
