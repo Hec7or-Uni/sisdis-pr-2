e080cb1175f8b46d7512f0ae0792372485c42597
hector toral <hectortoraltrabajos@gmail.com>
2022-10-19 10:20:31 +0200

    cmd

--- cmd/lector/main.go
+++ cmd/lector/main.go
@@ -1,5 +1,16 @@
 package main
 
-func main() {
+import (
+	"io/ioutil"
+	"sisdis-pr-2/cmd"
+)
 
+func LeerFichero() string {
+	data, err := ioutil.ReadFile("file.txt")
+	cmd.CheckError(err)
+	return string(data)
+}
+
+func main() {
+	LeerFichero()
 }
